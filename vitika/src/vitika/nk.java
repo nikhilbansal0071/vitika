7e265cfe21ccdf63c08fe8abbee9f104f7889764
nikhil1265 <nikhilbansal1265@gmail.com>
2023-04-08 15:42:08 -0400

    last commit

--- vitika/src/vitika/nk.java
+++ vitika/src/vitika/nk.java
@@ -1,10 +1,10 @@
 package vitika;
 
-public class nn {
+public class nk {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("fd  jj d");
-System.out.println("jjjww");
-	}}
 
+	}
+
+}
